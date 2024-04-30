7f03de4b49808fb544b4bae651ef92d1f4a76c67
nathanielbk <nathaniel.bjerke.kildal@gmail.com>
2024-04-30 12:56:54 +0200

    Three-way handshake draft

--- src/application.py
+++ src/application.py
@@ -1,32 +1,42 @@
 import socket
-import sys
+import argparse
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 8080
+# Set up argument parser
+parser = argparse.ArgumentParser(description='UDP client and server')
+parser.add_argument('--client', '-c', help='Client IP address')
+parser.add_argument('--server', '-s', help='Server IP address')
+parser.add_argument('--port', '-p', type=int, help='UDP port')
+parser.add_argument('--file', '-f', help='File name')
+args = parser.parse_args()
 
-if len(sys.argv) != 2:
-    print('Usage: python application.py -s|-c')
-    sys.exit()
+UDP_IP = args.server if args.server else "127.0.0.1"
+UDP_PORT = args.port if args.port else 8080
 
-if sys.argv[1] == '-c':
+if args.client:
     print('Client started...')
-    MESSAGE = "Hello World!"
-
     print("UDP target IP: %s" % UDP_IP)
     print("UDP target port: %s" % UDP_PORT)
-    print("Message: %s" % MESSAGE)
-
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-    sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
 
-elif sys.argv[1] == '-s':
-    print('Server startet...')
+    # Three-way handshake
+    sock.sendto(b'SYN', (UDP_IP, UDP_PORT))
+    data, addr = sock.recvfrom(1024)
+    if data == b'SYN-ACK':
+        sock.sendto(b'ACK', (UDP_IP, UDP_PORT))
+
+elif args.server:
+    print('Server started...')
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP 
     sock.bind((UDP_IP, UDP_PORT))
 
     while True: 
         data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        print("received message: %s" % data.decode())
-
+        if data == b'SYN':
+            sock.sendto(b'SYN-ACK', addr)
+            data, addr = sock.recvfrom(1024)
+            if data == b'ACK':
+                print('Connection Established (yey)')
+        else:
+            print("received message: %s" % data.decode())
 else:
     print('Invalid option. Use -s for server and -c for client.')
