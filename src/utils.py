05ec3ccdb55aebf30e735131fe8dd758bb58a2d2
nathanielbk <115590362+nathanielbk@users.noreply.github.com>
2024-05-14 20:24:01 +0200

    defualt ip update: 10.0.0.2

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='UDP client and server')
     parser.add_argument('--client', '-c', action='store_true', help='Run as client')
     parser.add_argument('--server', '-s', action='store_true', help='Run as server')
-    parser.add_argument('--ip', '-i', default='127.0.0.1', help='IP address of the server, default is 127.0.0.1')
+    parser.add_argument('--ip', '-i', default='10.0.0.2', help='IP address of the server, default is 127.0.0.1')
     parser.add_argument('--port', '-p', type=int, default=8080, help='UDP port, default is 8080, should be in range 1024-65535')
     parser.add_argument('--file', '-f', type=str, help='File name')
     parser.add_argument('--window', '-w', type=int, default=3, help='Sliding window size')
